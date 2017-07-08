8597563f0fabada424045167a1b0afe3ae5b31c4
8GutiMu <you@example.com>
2017-07-08 03:20:16 -0500

    agregando enlaces

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,20 @@
   $(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+    
   });
           
+
+
+var mostrarAceptado = function(){
+   swal("Registro correcto", "Empieza ahora", "success");
+    
+   setTimeout(function(){ location.href="../../proyectos.html" }, 3000);
+}
+
+var mostrarProyectos = function(){
+    setTimeout(function(){ location.href="../../tema2.html" }, 3000);
+}
+
+$("#form-registro").click(mostrarAceptado);
+$("#sumbmit-proyectos").click(mostrarProyectos);
