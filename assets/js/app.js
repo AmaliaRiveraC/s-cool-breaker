c146c91c3a77eea8b658012b0b1bcd17d75b1f0d
8GutiMu <you@example.com>
2017-07-08 13:43:37 -0500

    flujo ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
-  
-  });      
 
-var mostrarAceptado = function(){
-   swal("Registro correcto", "Empieza ahora", "success");
-    
-   setTimeout(function(){ location.href="../../proyectos.html" }, 3000);
+});
+
+var mostrarAceptado = function () {
+    swal("Registro correcto", "Empieza ahora", "success");
+
+    setTimeout(function () {
+        location.href = "proyectos.html"
+    }, 3000);
 }
 
-var mostrarProyectos = function(){
-    setTimeout(function(){ location.href="../../tema2.html" }, 3000);
+var mostrarProyectos = function () {
+    setTimeout(function () {
+        location.href = "tema2.html"
+    }, 3000);
+}
+
+var mostrarOk = function(event){
+    event.preventDefault();
+    swal("Registro correcto", "Empieza ahora", "success");
+
 }
 
 $("#form-registro").click(mostrarAceptado);
-$("#sumbmit-proyectos").click(mostrarProyectos);
+$("#sumbmit-proyectos").submit(mostrarok);
 
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 2 // Creates a dropdown of 15 years to control year
-    });
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 2 // Creates a dropdown of 15 years to control year
+});
 
 
 /* HTML feedback chat */
@@ -29,63 +39,61 @@ var comentarioEnTiempoReal = document.getElementById("comentarioEnTiempoReal");
 
 function duplicar() {
 
-  comentarioEnTiempoReal.innerHTML = ingresarComentario.value;
+    comentarioEnTiempoReal.innerHTML = ingresarComentario.value;
 
 }
 
 function publicarComentario() {
-  //Se comienza guardando el valor que el usurio ingrese en comentarioEnTiempoReal, clonando todos sus atributos, para así poderlo publicar d emanera identica.
+    //Se comienza guardando el valor que el usurio ingrese en comentarioEnTiempoReal, clonando todos sus atributos, para así poderlo publicar d emanera identica.
 
-  var comentarioPublicado = comentarioEnTiempoReal.cloneNode(true);
+    var comentarioPublicado = comentarioEnTiempoReal.cloneNode(true);
 
-  var nuevosComentarios = document.getElementById("comentariosPublicados");
+    var nuevosComentarios = document.getElementById("comentariosPublicados");
 
-  nuevosComentarios.insertBefore(comentarioPublicado, nuevosComentarios.firstChild);
+    nuevosComentarios.insertBefore(comentarioPublicado, nuevosComentarios.firstChild);
 
-  var lineaSeparadora = document.createElement("hr");
+    var lineaSeparadora = document.createElement("hr");
 
-  nuevosComentarios.insertBefore(lineaSeparadora, nuevosComentarios.firstChild);
-  comentarioEnTiempoReal.removeAttribute("style");
+    nuevosComentarios.insertBefore(lineaSeparadora, nuevosComentarios.firstChild);
+    comentarioEnTiempoReal.removeAttribute("style");
 }
 
 function letraChica() {
-  comentarioEnTiempoReal.style.fontSize = "20px";
+    comentarioEnTiempoReal.style.fontSize = "20px";
 
 }
 
 function letraMediana() {
-  comentarioEnTiempoReal.style.fontSize = "40px";
+    comentarioEnTiempoReal.style.fontSize = "40px";
 }
 
 function letraGrande() {
-  comentarioEnTiempoReal.style.fontSize = "80px";
+    comentarioEnTiempoReal.style.fontSize = "80px";
 }
 
 function colorDeTexto() {
-  var eleccionUsuario = prompt("Ingresa el código hexadecimal del color de texto que desees:  ");
-  eleccionUsuario = "#" + eleccionUsuario;
-  comentarioEnTiempoReal.style.color = eleccionUsuario;
+    var eleccionUsuario = prompt("Ingresa el código hexadecimal del color de texto que desees:  ");
+    eleccionUsuario = "#" + eleccionUsuario;
+    comentarioEnTiempoReal.style.color = eleccionUsuario;
 
 }
 
 function alinearIzquierda() {
-  comentarioEnTiempoReal.style.textAlign = "left";
+    comentarioEnTiempoReal.style.textAlign = "left";
 }
 
 function centrar() {
-  comentarioEnTiempoReal.style.textAlign = "center";
+    comentarioEnTiempoReal.style.textAlign = "center";
 }
 
 function alinearDerecha() {
-  comentarioEnTiempoReal.style.textAlign = "right";
+    comentarioEnTiempoReal.style.textAlign = "right";
 }
 
 
 function colorDeFondo() {
-  var eleccionUsuario = prompt("Ingresa el código hexadecimal del color de fondo que desees:  ");
-  eleccionUsuario = "#" + eleccionUsuario;
-  comentarioEnTiempoReal.style.backgroundColor = eleccionUsuario;
+    var eleccionUsuario = prompt("Ingresa el código hexadecimal del color de fondo que desees:  ");
+    eleccionUsuario = "#" + eleccionUsuario;
+    comentarioEnTiempoReal.style.backgroundColor = eleccionUsuario;
 
 }
-
-
